d18b6da503e556b174e8f96aea0cac9836a34a62
Rusinov Anton <rusinovantondev@gmail.com>
2018-05-31 20:31:48 +0300

    init

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
-  $("#slider").slick({
+  const slider = $('#slider');
+
+  slider.on('init', function(event, slick) {
+    requestAnimationFrame(function() {
+      slick.$slides[0].querySelector('video').play();
+    });
+  });
+
+  slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    slick.$slides[currentSlide].querySelector('video').pause();
+    slick.$slides[nextSlide].querySelector('video').play();
+  });
+
+  slider.slick({
     arrows: true,
-    centerMode: true,
-    // centerPadding: '60px',
-    slidesToShow: 3
+    infinite: true,
+    slidesToShow: 1,
+    swipe: false,
+    touchMove: false,
+    centerMode: !0,
+    centerPadding: '100px'
+    // variableWidth: true
   });
 });
